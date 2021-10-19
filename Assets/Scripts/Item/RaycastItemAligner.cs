9f308379f79ea2e46f04bcd4c03e78316c4021a5
zaiyaman <61441249+zaiyaman@users.noreply.github.com>
2021-10-19 14:25:12 -0400

    Make item spawner spawn items over ground collision
    Used raycast to spawn item at ground.

---
+++ Assets/Scripts/Item/RaycastItemAligner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastItemAligner : MonoBehaviour
+{
+    public float raycastDistance = 100f;
+    public GameObject objectToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PositionRaycast();
+    }
+
+    void PositionRaycast()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit, raycastDistance))
+        {
+            Quaternion spawnRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            GameObject clone = Instantiate(objectToSpawn, hit.point + new Vector3(0,3,0), spawnRotation);
+        }
+    }
+}
